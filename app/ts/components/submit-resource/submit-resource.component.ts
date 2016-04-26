279b3896175381c9c726508594a6794c46e8393a
Mark Norgate <mark.norgate@jisc.ac.uk>
2016-04-26 15:07:29 +0100

    Implement validation

--- app/ts/components/submit-resource/submit-resource.component.ts
+++ app/ts/components/submit-resource/submit-resource.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy, ViewChild } from 'angular2/core';
+import { ControlGroup, FormBuilder, AbstractControl, Validators } from 'angular2/common';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import { TagsService } from '../../services/services';
@@ -20,7 +21,7 @@ let _ = require('underscore');
     selector: 'submit-resource',
     template: require('./submit-resource.component.html'),
     styles: [require('./submit-resource.scss').toString()],
-    directives: [ TagCloudComponent, FileDrop, FileSelect ]
+    directives: [TagCloudComponent, FileDrop, FileSelect]
 })
 export class SubmitResourceComponent {
     @ViewChild('fileUploadButton') fileUploadButton;
@@ -31,10 +32,20 @@ export class SubmitResourceComponent {
     private _create: Subject<Tag> = new Subject<Tag>();
     private _remove: Subject<Tag> = new Subject<Tag>();
 
-    public uploader: FileUploader;
+    private submitResourceForm: ControlGroup;
+    private basicDetailsText: AbstractControl;
+    private overviewText: AbstractControl;
+    private descriptionText: AbstractControl;
+    private trialUrl: AbstractControl;
+    private videoUrl: AbstractControl;
+
+    protected uploader: FileUploader;
     
-    constructor() {
-        this.uploader = new FileUploader({ });
+    protected shaking: boolean = false;
+    protected submitted: boolean = false;
+
+    constructor(fb: FormBuilder) {
+        this.uploader = new FileUploader({});
 
         this._resourceTags = this._tagUpdates
             .scan((tags: Tag[], operation: ITagsOperation) => operation(tags), initialTags);
@@ -48,21 +59,60 @@ export class SubmitResourceComponent {
             .subscribe(this._tagUpdates);
 
         this._newResourceTags.subscribe(this._create);
+
+        this.buildForm(fb);
     }
 
     get resourceTags(): Observable<Tag[]> {
         return this._resourceTags;
-    } 
-    
+    }
+
+    protected buildForm(fb: FormBuilder) {
+        this.submitResourceForm = fb.group({
+            "basicDetailsText": ["", Validators.required],
+            "overviewText": ["", Validators.required],
+            "descriptionText": ["", Validators.required],
+            "trialUrl": ["", Validators.required],
+            "videoUrl": ["", Validators.required]
+        })
+
+        this.basicDetailsText = this.submitResourceForm.controls['basicDetailsText'];
+        this.overviewText = this.submitResourceForm.controls['overviewText'];
+        this.descriptionText = this.submitResourceForm.controls['descriptionText'];
+        this.trialUrl = this.submitResourceForm.controls['trialUrl'];
+        this.videoUrl = this.submitResourceForm.controls['videoUrl'];
+    }
+
     protected addTagToResource(tag: Tag): void {
         this._create.next(tag);
     }
-    
+
     protected removeTagFromResource(tag: Tag): void {
         this._remove.next(tag);
     }
-    
+
     protected uploadFileButtonClicked() {
         this.fileUploadButton.nativeElement.click();
+    }
+
+    protected onSubmit(formValues: any) {
+        this.submitted = true;
+        
+        if (this.submitResourceForm.valid) {
+            this.submitResource();
+        } else {
+            this.shakeForm();
+        }
+    }
+    
+    protected submitResource() {
+        
+    }
+
+    protected shakeForm() {
+        this.shaking = true;
+        setTimeout(() => {
+            this.shaking = false;
+        }, 500);
     }
 }
