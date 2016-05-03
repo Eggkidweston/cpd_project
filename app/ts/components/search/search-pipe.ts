1d7e6da2918a275ea7ab60314a4a5ee913455557
Mark Norgate <mark.norgate@jisc.ac.uk>
2016-05-03 10:18:06 +0100

    Fix pipe issue on homepage search

--- app/ts/components/search/search-pipe.ts
+++ app/ts/components/search/search-pipe.ts
@@ -1,16 +1,13 @@
-// import { Pipe } from "angular2/core";
-// import { StoreApp } from '../../models';
+import { Pipe } from "angular2/core";
+import { StoreApp } from '../../models';
 
-// @Pipe({
-// 	name: "search"
-// })
-
-// export class SearchPipe{
-// 	transform(value, [term]) {
-// 		if (value && term) {
-// 			return value.filter( item => item.title.toUpperCase().includes(term.toUpperCase()) );
-// 		} else {
-// 			return value;
-// 		}
-// 	}
-// }
+@Pipe({ name: "search" })
+export class SearchPipe{
+	transform(value, term) {
+		if (value && term) {
+			return value.filter( item => item.title.toUpperCase().includes(term.toUpperCase()) );
+		} else {
+			return value;
+		}
+	}
+}
