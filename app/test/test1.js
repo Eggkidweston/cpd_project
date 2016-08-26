dc254ae9fc3de0ab6f8b44e4f35579debaabbb16
Jonny Gibson <jonnygibson@Jonnys-MacBook.local>
2016-08-26 12:30:02 +0100

    update mocha test folder

--- app/test/test1.js
+++ app/test/test1.js
@@ -2,7 +2,6 @@
  * Created by jonnygibson on 26/08/2016.
  */
 var expect                               =  require("chai").expect;
-
 describe('Index Page', function() {
     it('Add 1+1 correctly', function () {
         var onePlusOne = 1 + 1;
