32f83cb589001600faacb312911e9439df89f340
Jonny Gibson <jonnygibson@Jonnys-MacBook.local>
2016-08-26 12:10:09 +0100

    ignore tests for CI purposes

---
+++ app/test/test1.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jonnygibson on 26/08/2016.
+ */
+var expect                               =  require("chai").expect;
+
+describe('Index Page', function() {
+    it('Add 1+1 correctly', function () {
+        var onePlusOne = 1 + 1;
+        expect(onePlusOne).to.equal(2);
+    });
+})
