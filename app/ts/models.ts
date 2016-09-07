94bdaee51e9ff06084224e8c8daaed65b3aff4ee
Justin Haylock <justin.haylock@jisc.ac.uk>
2016-09-07 15:14:30 +0100

    Adds link in terms and removes bad code

--- app/ts/models.ts
+++ app/ts/models.ts
@@ -108,7 +108,7 @@ export class TagCloud
     public GetFilterSyntax(){
         var filter = "";
         for (let tag of this.Tags) {
-            filter += "(tag eq '"+tag.name.toUpperCase()+"')";
+            filter += "(tag eq '"+tag.name+"')";
             if (tag != this.Tags[this.Tags.length -1]){
                 filter += " and ";
             }
