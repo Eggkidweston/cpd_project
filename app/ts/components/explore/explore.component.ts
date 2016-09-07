4c9b7d6ac174be7634bbc5748cf91b95b258837d
Justin Haylock <justin.haylock@jisc.ac.uk>
2016-09-07 17:35:06 +0100

    Fixes bug with choosing a tag from a resource details page

--- app/ts/components/explore/explore.component.ts
+++ app/ts/components/explore/explore.component.ts
@@ -28,8 +28,6 @@ require("../../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js
 })
 export class ExploreComponent implements AfterViewInit {
 
-    public resourceId: number;
-
     private storeApps: Array<StoreApp>;
     public errorMessage: string;
 
@@ -46,7 +44,7 @@ export class ExploreComponent implements AfterViewInit {
                 public appsService: AppsService,
                 public cdr: ChangeDetectorRef,
                 params: RouteParams) {
-        this.resourceId = +params.get('id');
+        
         this.queryTags = params.get('tags');
         this.chosenOrder = "pop";
 
@@ -56,9 +54,11 @@ export class ExploreComponent implements AfterViewInit {
 
     ngAfterViewInit() {
         if (this.queryTags) {
+            //console.log('loadCloud ngAfterViewInitA');
             this.loadCloud(this.queryTags);
         }
         else {
+            //console.log('loadCloud ngAfterViewInitB');
             this.loadCloud();
         }
     }
@@ -77,26 +77,44 @@ export class ExploreComponent implements AfterViewInit {
         var selectedTag = this.tagcloud.GetTag(tagId);
         this.selectedTags = this.selectedTags || new TagCloud([]);
         this.selectedTags.AddTag(selectedTag);
+       // console.log('loadCloud selectTag');
         this.loadCloud();
+    }
 
+    selectTagFromDB(tagId) {
+        this.appsService.getTags(tagId)
+            .subscribe(
+                tags => {
+                        this.gettingTags = false;
+                        this.selectedTags = new TagCloud([tags[0]]);
+                        this.loadCloud();
+                        this.loadApps();
+
+                    },
+                (error: any) => AppComponent.generalError(error.status)
+            );
     }
 
     removeTag(tagId) {
         this.selectedTags.RemoveTag(tagId);
+        
         this.loadCloud();
     }
 
     order(order) {
         this.chosenOrder = order;
         this.selectedTags = new TagCloud([]);
+        
         this.loadCloud();
     }
 
-    loadApps() { //TODO: fix the API availablerows number that's being returned!
-        this.appsService.getResources(1500, 1, this.selectedTags.GetFilterSyntax())
+    loadApps() {
+        //console.log('loading apps');
+        this.appsService.getResources(900, 1, this.selectedTags.GetFilterSyntax())
             .subscribe(
                 storeApps => {
                     this.storeApps = storeApps.data;
+                    //console.log(this.storeApps);
                 },
                 (error: any) => AppComponent.generalError(error.status)
             );
@@ -113,34 +131,47 @@ export class ExploreComponent implements AfterViewInit {
     }
 
     loadCloud(initialSelectedId = null) {
+        //console.log(initialSelectedId);
+        //console.log(this.selectedTags);
         this.gettingTags = true;
         if (this.selectedTags.Tags.length > 0) {
+            //console.log('use selectedTags');
             var tagIds = this.selectedTags.GetIds();
             this.appsService.getRelatedTags(tagIds)
                 .subscribe(
                     tags => {
                         this.gettingTags = false;
                         this.tagcloud = new TagCloud(tags);
+
                         this.loadApps();
                     },
                     (error: any) => AppComponent.generalError(error.status)
                 );
         }
         else {
+            if (initialSelectedId != null){
+                //console.log('use initialSelectedId');
+                this.selectTagFromDB(initialSelectedId);
+            } else {
+
+            
+            
+
+
             this.appsService.getTagCloud(true, 50, this.chosenOrder)
                 .subscribe(
                     tags => {
+                        //console.log('getTagCloud');
+                        //console.log(tags);
                         this.gettingTags = false;
                         this.tagcloud = new TagCloud(tags);
-                        if (initialSelectedId != null){
-                            this.selectTag(initialSelectedId)
-                        }
-                        else {
-                            this.loadRecomendedApps();
-                        }
+                        this.loadRecomendedApps();
+                        
                     },
                     (error: any) => AppComponent.generalError(error.status)
                 );
+
+            }
         }
         error => this.errorMessage = <any>error;
 
