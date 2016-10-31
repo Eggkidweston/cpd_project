0c596d97a164470784cfd6b14c02de746b168d2d
Jonny Gibson <jonny@jgibson.com>
2016-10-31 13:57:27 +0000

    Change api end point to https

--- configs/live.ts
+++ configs/live.ts
@@ -1,5 +1,5 @@
 export var appSettings = {
-    apiRoot: 'http://store.data.alpha.jisc.ac.uk:8080/',
+    apiRoot: 'https://store.data.alpha.jisc.ac.uk:8080/',
     curationRoot: 'https://curation.data.alpha.jisc.ac.uk/',
     s3Root: 'https://s3-eu-west-1.amazonaws.com/jisc-store-resources/',
     idpMembers: 'https://microservice.data.alpha.jisc.ac.uk:1337/idps'
