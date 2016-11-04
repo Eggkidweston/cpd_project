7eab5008e433b93b7488a70065ecd6676baabb40
Jonny Gibson <jonnygibson@Jonnys-MacBook.local>
2016-11-04 15:14:30 +0000

    update live.ts to point at curation on staging

--- configs/live.ts
+++ configs/live.ts
@@ -1,6 +1,6 @@
 export var appSettings = {
     apiRoot: 'http://store.data.alpha.jisc.ac.uk:8080/',
-    curationRoot: 'https://curation.data.alpha.jisc.ac.uk/',
+    curationRoot: 'https://curation-staging.data.alpha.jisc.ac.uk/',
     s3Root: 'https://s3-eu-west-1.amazonaws.com/jisc-store-resources/',
     idpMembers: 'https://microservice.data.alpha.jisc.ac.uk:1337/idps'
 };
