f81cbdfabb2d1b3e79eb06bfb838d8998988aae0
Jonny Gibson <jonnygibson@Jonnys-MacBook.local>
2017-02-15 15:41:13 +0000

    update live config, removing port specification

--- configs/live.ts
+++ configs/live.ts
@@ -1,5 +1,5 @@
 export var appSettings = {
-    apiRoot: 'https://store.data.alpha.jisc.ac.uk:8080/',
+    apiRoot: 'https://api.store.jisc.ac.uk/',
     curationRoot: 'https://curation-staging.data.alpha.jisc.ac.uk/',
     s3Root: 'https://s3-eu-west-1.amazonaws.com/jisc-store-resources/',
     idpMembers: 'https://microservice.data.alpha.jisc.ac.uk:1337/idps'
