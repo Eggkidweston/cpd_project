1b147facef5b56a4b38264b789f41ae17642ad27
Justin Haylock <justin.haylock@jisc.ac.uk>
2017-06-27 15:59:51 +0100

    Updates release notes and version number

--- app/ts/services/services.ts
+++ app/ts/services/services.ts
@@ -23,4 +23,4 @@ export var servicesInjectables: Array<any> = [
     idpRegisterServiceInjectables
 ];
 
-export var appInfo = { name: 'Store', strap: 'Open and commercial educational apps and resources', version: 'version 1.3.2'}
+export var appInfo = { name: 'Store', strap: 'Open and commercial educational apps and resources', version: 'version 1.4'}
